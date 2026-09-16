80b22e5cf2dbd0edd95ba459475a7e62abd99b94
Pezhman Arzandeh <20170213@tafe.wa.edu.au>
2026-09-16 11:26:23 +0800

    change the sturcture

--- weather.py
+++ weather.py
@@ -18,3 +18,21 @@ temps = {
 with open(fullpath, "w") as out_file:
     for city, temperature in temps.items():
         out_file.write(f"{city}:{temperature}\n")
+
+weather_dictionary = dict()
+weather_list = list()
+
+with open(fullpath, "r") as in_file:
+    all_lines = in_file.readlines()
+
+    for line in all_lines:
+        line = line.strip()
+
+        weather_list = line.split(":")
+
+        city = weather_list[0]
+        temperature = int(weather_list[1])
+
+        weather_dictionary[city] = temperature
+
+print(weather_dictionary)
